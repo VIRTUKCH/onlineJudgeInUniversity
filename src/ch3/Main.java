c9047a510856f6430e45668bf2f7b3325396c1fc
kch4731 <kch4731@naver.com>
2022-10-22 15:22:48 +0900

    [Update : Update new source codes]

--- src/ch3/Main.java
+++ src/ch3/Main.java
@@ -1,155 +1,155 @@
-package ch3;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class Main {
-	public static void printArray(double arr[][]) {
-		System.out.println("arr length: " + arr.length);
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print("arr[" + i + "] ");
-			for (int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
-
-	public static double[][] run1(Scanner s) {
-		System.out.print("The number of rows of non-square array of doubles to create? ");
-		int n = s.nextInt();
-		double dArr[][] = new double[n][];
-		for (int i = 0; i < n; i++) {
-			dArr[i] = new double[i + 1];
-			System.out.print("Input continuously " + (i + 1) + " doubles to be stored in line " + (i + 1) + ": ");
-			for (int j = 0; j <= i; j++) {
-				dArr[i][j] = s.nextDouble();
-			}
-		}
-		return dArr;
-	}
-
-	private static double[][] run2(Scanner s) { // run1 -> 예외처리 -> run2
-		int n = 1;
-		while (true) {
-			try {
-				System.out.print("The number of rows of non-square array of doubles to create? ");
-				n = s.nextInt();
-				double dArr[][] = new double[n][]; // 배열을 꼭 만들어줘야 했어. 2차원 배열 안하고 1차원 배열 했어도 되긴 해
-				break;
-			} catch (NegativeArraySizeException e) { // 이게 양수가 아니라고 난리
-				System.out.println("Input a POSITIVE integer. Try again.");
-			} catch (InputMismatchException e) { // 이건 괜찮은데
-				System.out.println("Input an INTEGER. Try again.");
-				s.next(); // 정수가 아닌 값을 넣으면 버퍼에서 꺼내주지 않기 때문에 무한히 반복할 수 있다. -> 제거
-			}
-		}
-
-		double dArr[][] = new double[n][];
-
-		for (int i = 0; i < n; i++) {
-			dArr[i] = new double[i + 1]; // 메모리 할당을 해준다. 1개, 2개, 3개...
-			while (true) {
-				try {
-					System.out
-							.print("Input continuously " + (i + 1) + " doubles to be stored in line " + (i + 1) + ": ");
-					for (int j = 0; j <= i; j++) {
-						dArr[i][j] = s.nextDouble();
-					}
-					break;
-				} catch (InputMismatchException e) { // 예외가 발생했을 때,
-					System.out.println("Input a integer or double. Try again.");
-					s.nextLine();
-				}
-			}
-		}
-		return dArr;
-	}
-
-	public static void run3(Scanner s) {
-		final int USER = 0; // 상수 값 정의
-		final int COMPUTER = 1;
-
-		// 0, 1, 2 중 하나의 난수를 미리 발생하여 저장해 놓은 난수 배열
-		// 이 배열에 저장된 값은 나중에 MJBarray[] 배열의 인덱스 값으로 사용됨
-		int randArray[] = { 2, 1, 2, 2, 2, 1, 1, 0, 0, 2, 0, 2, 0, 2, 0, 1, 1, 2, 0, 0, 2, 0, 2, 2, 1, 2, 0, 0, 1, 2, };
-
-		String MJBarray[] = { "m", "j", "b" }; // 묵(m)찌(j)빠(b) 문자열을 가진 배열
-
-		System.out.println("Start the MUK-JJI-BBA game.");
-		System.out.print("Select any index for random number[0~29]? ");
-		int randIdx = s.nextInt();
-		s.nextLine(); // 입력 버퍼에 있는 '\n' 제거
-
-		int priority = USER; // 누가 우선권을 가졌는지 저장하고 있음, USER:사용자 우선권, COMPUTER:computer 우선권
-		String priStr[] = { "USER", "COMPUTER" }; // 화면에 출력할 때 사용함
-
-		while (true) {
-			System.out.println();
-			System.out.println(priStr[priority] + " has the higher priotiy."); // 화면에 (priStr[priority] + " has the
-																				// higher priotiy.")를 출력
-			System.out.print("m(muk), j(jji), b(bba) or stop? "); // 화면에 "m(muk), j(jji), b(bba) or stop? " 출력
-
-			String user = s.nextLine(); // 사용자가 입력한 묵찌빠 문자열을 입력 받아 String user 변수에 저장
-			if (user.equals("stop")) { // 사용자가 입력한 문자열을 비교할 때는 if (user.equals("stop")) 문장을 사용
-				return; // "stop"을 입력했으면 게임 종료
-			} else if (!user.equals("m") && !user.equals("j") && !user.equals("b")) { // 사용자가 m, j, b를 입력하지 않고 다른 문자열을
-																						// 입력했다면
-				System.out.println("Select one among m, j, b."); // 아래 실행결과를 참고하여 적절한 에러 메시지를 출력하고 while()의 처음부터 다시 시작
-			}
-
-			int comIdx = randArray[(randIdx++) % 30]; // 사용자가 randIdx를 고른다.
-			// 컴퓨터가 사용하는 난수 배열 인덱스 값 randIdx를 1 증가시킨 후 29보다 큰 경우 다시 0부터 시작 -> 이게 이걸 원한건지는
-			// 모르겠어.
-
-			String computer = MJBarray[comIdx]; // MJBarray[comIdx](컴퓨터가 낸 묵찌빠 문자열)을 String computer 변수에 저장
-			System.out.print("User = " + user + ", Computer = " + computer + ", ");
-//            user와 computer 변수를 이용하여 화면에 사용자, 컴퓨터가 낸 묵찌빠 값을 출력 (예:User = m, Computer = b, )
-
-			if (user.equals(computer)) { // 이후 user와 computer 문자열을 비교하여 같으면
-				// user와 computer 두 문자열 비교하는 방법: if (user.equals(computer))
-				System.out.println(priStr[priority] + " WINs."); // 우선권을 가진 자가 이겼다고 출력하고
-			} else { // 같지 않으면(비겼으면)
-				System.out.println("SAME."); // 비겼다고 출력
-			}
-
-//                현 상황이 (가위바위보)라 가정하고 누가 이겼는지 판단하여 우선권을 가지게 함 
-//                (아주 긴 조건문이 될 것임) // 묵찌빠의 경우의수를 다 구현해야 해서가 아닐까
-//                priority 값을 이긴 자(USER 또는 COMPUTER)로 변경함
-			if (user.equals("m") && computer.equals("j")) {
-				priority = USER;
-			} else if (user.equals("j") && computer.equals("b")) {
-				priority = USER;
-			} else if (user.equals("b") && computer.equals("m")) {
-				priority = USER;
-			} else if (user.equals("m") && computer.equals("b")) {
-				priority = COMPUTER;
-			} else if (user.equals("j") && computer.equals("m")) {
-				priority = COMPUTER;
-			} else if (user.equals("m") && computer.equals("j")) {
-				priority = COMPUTER;
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		double array[][] = { { 0 }, { 1, 2 }, { 3, 4, 5 } };
-		printArray(array);
-		System.out.println();
-
-		Scanner scanner = new Scanner(System.in); // 필요한 파일 import시킬 것
-		double dArr1[][] = run1(scanner);
-		printArray(dArr1);
-		System.out.println();
-
-		double dArr2[][] = run2(scanner);
-		printArray(dArr2);
-		System.out.println();
-
-		run3(scanner);
-		System.out.println();
-
-		scanner.close();
-		System.out.println("Done.");
-	}
-}
+//package ch3;
+//
+//import java.util.InputMismatchException;
+//import java.util.Scanner;
+//
+//public class Main {
+//	public static void printArray(double arr[][]) {
+//		System.out.println("arr length: " + arr.length);
+//		for (int i = 0; i < arr.length; i++) {
+//			System.out.print("arr[" + i + "] ");
+//			for (int j = 0; j < arr[i].length; j++) {
+//				System.out.print(arr[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+//	}
+//
+//	public static double[][] run1(Scanner s) {
+//		System.out.print("The number of rows of non-square array of doubles to create? ");
+//		int n = s.nextInt();
+//		double dArr[][] = new double[n][];
+//		for (int i = 0; i < n; i++) {
+//			dArr[i] = new double[i + 1];
+//			System.out.print("Input continuously " + (i + 1) + " doubles to be stored in line " + (i + 1) + ": ");
+//			for (int j = 0; j <= i; j++) {
+//				dArr[i][j] = s.nextDouble();
+//			}
+//		}
+//		return dArr;
+//	}
+//
+//	private static double[][] run2(Scanner s) { // run1 -> 예외처리 -> run2
+//		int n = 1;
+//		while (true) {
+//			try {
+//				System.out.print("The number of rows of non-square array of doubles to create? ");
+//				n = s.nextInt();
+//				double dArr[][] = new double[n][]; // 배열을 꼭 만들어줘야 했어. 2차원 배열 안하고 1차원 배열 했어도 되긴 해
+//				break;
+//			} catch (NegativeArraySizeException e) { // 이게 양수가 아니라고 난리
+//				System.out.println("Input a POSITIVE integer. Try again.");
+//			} catch (InputMismatchException e) { // 이건 괜찮은데
+//				System.out.println("Input an INTEGER. Try again.");
+//				s.next(); // 정수가 아닌 값을 넣으면 버퍼에서 꺼내주지 않기 때문에 무한히 반복할 수 있다. -> 제거
+//			}
+//		}
+//
+//		double dArr[][] = new double[n][];
+//
+//		for (int i = 0; i < n; i++) {
+//			dArr[i] = new double[i + 1]; // 메모리 할당을 해준다. 1개, 2개, 3개...
+//			while (true) {
+//				try {
+//					System.out
+//							.print("Input continuously " + (i + 1) + " doubles to be stored in line " + (i + 1) + ": ");
+//					for (int j = 0; j <= i; j++) {
+//						dArr[i][j] = s.nextDouble();
+//					}
+//					break;
+//				} catch (InputMismatchException e) { // 예외가 발생했을 때,
+//					System.out.println("Input a integer or double. Try again.");
+//					s.nextLine();
+//				}
+//			}
+//		}
+//		return dArr;
+//	}
+//
+//	public static void run3(Scanner s) {
+//		final int USER = 0; // 상수 값 정의
+//		final int COMPUTER = 1;
+//
+//		// 0, 1, 2 중 하나의 난수를 미리 발생하여 저장해 놓은 난수 배열
+//		// 이 배열에 저장된 값은 나중에 MJBarray[] 배열의 인덱스 값으로 사용됨
+//		int randArray[] = { 2, 1, 2, 2, 2, 1, 1, 0, 0, 2, 0, 2, 0, 2, 0, 1, 1, 2, 0, 0, 2, 0, 2, 2, 1, 2, 0, 0, 1, 2, };
+//
+//		String MJBarray[] = { "m", "j", "b" }; // 묵(m)찌(j)빠(b) 문자열을 가진 배열
+//
+//		System.out.println("Start the MUK-JJI-BBA game.");
+//		System.out.print("Select any index for random number[0~29]? ");
+//		int randIdx = s.nextInt();
+//		s.nextLine(); // 입력 버퍼에 있는 '\n' 제거
+//
+//		int priority = USER; // 누가 우선권을 가졌는지 저장하고 있음, USER:사용자 우선권, COMPUTER:computer 우선권
+//		String priStr[] = { "USER", "COMPUTER" }; // 화면에 출력할 때 사용함
+//
+//		while (true) {
+//			System.out.println();
+//			System.out.println(priStr[priority] + " has the higher priotiy."); // 화면에 (priStr[priority] + " has the
+//																				// higher priotiy.")를 출력
+//			System.out.print("m(muk), j(jji), b(bba) or stop? "); // 화면에 "m(muk), j(jji), b(bba) or stop? " 출력
+//
+//			String user = s.nextLine(); // 사용자가 입력한 묵찌빠 문자열을 입력 받아 String user 변수에 저장
+//			if (user.equals("stop")) { // 사용자가 입력한 문자열을 비교할 때는 if (user.equals("stop")) 문장을 사용
+//				return; // "stop"을 입력했으면 게임 종료
+//			} else if (!user.equals("m") && !user.equals("j") && !user.equals("b")) { // 사용자가 m, j, b를 입력하지 않고 다른 문자열을
+//																						// 입력했다면
+//				System.out.println("Select one among m, j, b."); // 아래 실행결과를 참고하여 적절한 에러 메시지를 출력하고 while()의 처음부터 다시 시작
+//			}
+//
+//			int comIdx = randArray[(randIdx++) % 30]; // 사용자가 randIdx를 고른다.
+//			// 컴퓨터가 사용하는 난수 배열 인덱스 값 randIdx를 1 증가시킨 후 29보다 큰 경우 다시 0부터 시작 -> 이게 이걸 원한건지는
+//			// 모르겠어.
+//
+//			String computer = MJBarray[comIdx]; // MJBarray[comIdx](컴퓨터가 낸 묵찌빠 문자열)을 String computer 변수에 저장
+//			System.out.print("User = " + user + ", Computer = " + computer + ", ");
+////            user와 computer 변수를 이용하여 화면에 사용자, 컴퓨터가 낸 묵찌빠 값을 출력 (예:User = m, Computer = b, )
+//
+//			if (user.equals(computer)) { // 이후 user와 computer 문자열을 비교하여 같으면
+//				// user와 computer 두 문자열 비교하는 방법: if (user.equals(computer))
+//				System.out.println(priStr[priority] + " WINs."); // 우선권을 가진 자가 이겼다고 출력하고
+//			} else { // 같지 않으면(비겼으면)
+//				System.out.println("SAME."); // 비겼다고 출력
+//			}
+//
+////                현 상황이 (가위바위보)라 가정하고 누가 이겼는지 판단하여 우선권을 가지게 함 
+////                (아주 긴 조건문이 될 것임) // 묵찌빠의 경우의수를 다 구현해야 해서가 아닐까
+////                priority 값을 이긴 자(USER 또는 COMPUTER)로 변경함
+//			if (user.equals("m") && computer.equals("j")) {
+//				priority = USER;
+//			} else if (user.equals("j") && computer.equals("b")) {
+//				priority = USER;
+//			} else if (user.equals("b") && computer.equals("m")) {
+//				priority = USER;
+//			} else if (user.equals("m") && computer.equals("b")) {
+//				priority = COMPUTER;
+//			} else if (user.equals("j") && computer.equals("m")) {
+//				priority = COMPUTER;
+//			} else if (user.equals("m") && computer.equals("j")) {
+//				priority = COMPUTER;
+//			}
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//		double array[][] = { { 0 }, { 1, 2 }, { 3, 4, 5 } };
+//		printArray(array);
+//		System.out.println();
+//
+//		Scanner scanner = new Scanner(System.in); // 필요한 파일 import시킬 것
+//		double dArr1[][] = run1(scanner);
+//		printArray(dArr1);
+//		System.out.println();
+//
+//		double dArr2[][] = run2(scanner);
+//		printArray(dArr2);
+//		System.out.println();
+//
+//		run3(scanner);
+//		System.out.println();
+//
+//		scanner.close();
+//		System.out.println("Done.");
+//	}
+//}
--- src/ch4/Main2.java
+++ src/ch4/Main2.java
@@ -1,172 +1,172 @@
-package ch4;
-
-import java.util.Scanner;
-
-class Order {
-	int[] selectMenuNums; // 메뉴번호를 저장하는 정수배열. 사용자의 입장에서 고른 것임
-	int[] sizeOfOrders; // 주문 수량을 저장하는 정수배열. 생성 말고 선언만 하자
-	Menuboard menuboard;
-	int cnt = 0; // 어디다 쓰는지 잘 모르겠는데, index에 쓰이지 않을까 해서 static으로 저장해봄
-
-//	Order() { // 멤버변수에 배열을 생성하지는 말라고 하니, 생성자에서 배열을 생성하는게 맞겠지? -> 잘 모르겠고 그냥 멤버변수 선언하면서 같이 생성해버렸음
-//		selectMenuNums = new int[8];
-//		sizeOfOrders = new int[8];
+//package ch4;
+//
+//import java.util.Scanner;
+//
+//class Order { // 4-2에서 새로 만든 클래스
+//	int[] selectMenuNums; // 메뉴번호를 저장하는 정수배열. 사용자의 입장에서 고른 것임
+//	int[] sizeOfOrders; // 주문 수량을 저장하는 정수배열. 생성 말고 선언만 하자
+//	Menuboard menuboard;
+//	int cnt = 0; // 어디다 쓰는지 잘 모르겠는데, index에 쓰이지 않을까 해서 static으로 저장해봄
+//
+////	Order() { // 멤버변수에 배열을 생성하지는 말라고 하니, 생성자에서 배열을 생성하는게 맞겠지? -> 잘 모르겠고 그냥 멤버변수 선언하면서 같이 생성해버렸음
+////		selectMenuNums = new int[8];
+////		sizeOfOrders = new int[8];
+////	}
+//
+//	Order(int sizeOfOrders, Menuboard menuboard) { // 주분의 개수와 Menubaord를 넘겨 받아 멤버에 저장한다.
+//		this.menuboard = menuboard; // 내가 가지고 있는 menuboard와 매개변수의 menuboard를 일치시킨다
+//		this.selectMenuNums = new int[Menuboard.menus.length]; // 메뉴번호를 저장하는 정수배열
+//		this.sizeOfOrders = new int[Menuboard.menus.length]; // 주문의 수량을 저장하는 정수배열.
+//		cnt = 0; // cnt는 0으로 초기화하라고 했음 -> 어디다 쓰는지는 잘 모르겠음
 //	}
-
-	Order(int sizeOfOrders, Menuboard menuboard) { // 주분의 개수와 Menubaord를 넘겨 받아 멤버에 저장한다.
-		this.menuboard = menuboard; // 내가 가지고 있는 menuboard와 매개변수의 menuboard를 일치시킨다
-		this.selectMenuNums = new int[Menuboard.menus.length]; // 메뉴번호를 저장하는 정수배열
-		this.sizeOfOrders = new int[Menuboard.menus.length]; // 주문의 수량을 저장하는 정수배열.
-		cnt = 0; // cnt는 0으로 초기화하라고 했음 -> 어디다 쓰는지는 잘 모르겠음
-	}
-
-	public boolean addMenu(int menuNum, int sizeOfOrders) {
-		if (menuNum < 0 || menuNum > 8) {
-			System.out.println("Menu id " + menuNum + " doesn't exist in our menuboard");
-			return false;
-		} else {
-			this.selectMenuNums[cnt] = menuNum;
-			this.sizeOfOrders[cnt] = sizeOfOrders;
-			cnt++;
-			System.out.println("Your selected menu is added to the order");
-			return true;
-		}
-	}
-
-	public void print() {
-		System.out.println("** Order details **");
-
-		for (int index = 0; index < this.cnt; index++) {
-
-			int indexOfOrder = this.selectMenuNums[index] - 1; // 실제 메뉴번호 -1
-//			int price = (int) (this.menuboard[index].getPrice());
-//			int price = (int) (this.menuboard.prices[index]) * this.sizeOfOrders[index];
-			int price = (int) (this.menuboard.prices[indexOfOrder]) * this.sizeOfOrders[index];
-
-//			 메뉴번호와 주문수량을 저장하는 배열의 내용을 순서적으로 출력한다. 가격도 계산하여 함께 출력할 것.
-			System.out.println("Menu: " + this.menuboard.menus[indexOfOrder] + " Qty: " + this.sizeOfOrders[index]
-					+ " Price: " + price);
-
-		}
-	}
-}
-//====================================================================================/
-//MenuOrder =========================================================================/
-
-class MenuOrder {
-	public static void makeOrder() {
-		Menuboard menuboard = Menuboard.makeBoard(); // Menuboard는 생성자가 private이니까
-		Menuboard.print(); // Best Coffee 부분 출력함
-		Scanner sc = new Scanner(System.in);
-		System.out.print("How many kinds of drinks? ");
-		int n = sc.nextInt();
-
-		Order order = new Order(n, menuboard);
-
-//		while (Order.cnt != n) {
+//
+//	public boolean addMenu(int menuNum, int sizeOfOrders) {
+//		if (menuNum < 0 || menuNum > 8) {
+//			System.out.println("Menu id " + menuNum + " doesn't exist in our menuboard");
+//			return false;
+//		} else {
+//			this.selectMenuNums[cnt] = menuNum;
+//			this.sizeOfOrders[cnt] = sizeOfOrders;
+//			cnt++;
+//			System.out.println("Your selected menu is added to the order");
+//			return true;
+//		}
+//	}
+//
+//	public void print() {
+//		System.out.println("** Order details **");
+//
+//		for (int index = 0; index < this.cnt; index++) {
+//
+//			int indexOfOrder = this.selectMenuNums[index] - 1; // 실제 메뉴번호 -1
+////			int price = (int) (this.menuboard[index].getPrice());
+////			int price = (int) (this.menuboard.prices[index]) * this.sizeOfOrders[index];
+//			int price = (int) (this.menuboard.prices[indexOfOrder]) * this.sizeOfOrders[index];
+//
+////			 메뉴번호와 주문수량을 저장하는 배열의 내용을 순서적으로 출력한다. 가격도 계산하여 함께 출력할 것.
+//			System.out.println("Menu: " + this.menuboard.menus[indexOfOrder] + " Qty: " + this.sizeOfOrders[index]
+//					+ " Price: " + price);
+//
+//		}
+//	}
+//}
+////====================================================================================/
+////MenuOrder =========================================================================/
+//
+//class MenuOrder { // 4-2에서 새로 만든 클래스
+//	public static void makeOrder() {
+//		Menuboard menuboard = Menuboard.makeBoard(); // Menuboard는 생성자가 private이니까
+//		Menuboard.print(); // Best Coffee 부분 출력함
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("How many kinds of drinks? ");
+//		int n = sc.nextInt();
+//
+//		Order order = new Order(n, menuboard);
+//
+////		while (Order.cnt != n) {
+////			System.out.print("Menu no? ");
+////			int menu_no = sc.nextInt();
+////			System.out.print("Quantity? ");
+////			int quantity = sc.nextInt();
+////			order.addMenu(menu_no, quantity);
+////		}
+//
+//		for (int i = 0; i < n; i++) {
 //			System.out.print("Menu no? ");
 //			int menu_no = sc.nextInt();
 //			System.out.print("Quantity? ");
 //			int quantity = sc.nextInt();
-//			order.addMenu(menu_no, quantity);
+//			if (!order.addMenu(menu_no, quantity)) {
+//				i = i - 1;
+//			}
 //		}
-
-		for (int i = 0; i < n; i++) {
-			System.out.print("Menu no? ");
-			int menu_no = sc.nextInt();
-			System.out.print("Quantity? ");
-			int quantity = sc.nextInt();
-			if (!order.addMenu(menu_no, quantity)) {
-				i = i - 1;
-			}
-		}
-
-//		System.out.println(Arrays.toString(order.sizeOfOrders));
-		order.print();
-		sc.close();
-	}
-}
-
-//====================================================================================/
-//Menu =========================================================================/
-
-class Menu {
-	private int id; // 메뉴의 번호
-	private String name; // 메뉴의 이름
-	private double price; // 메뉴의 가격
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public void print() {
-		System.out.println(id + ": " + name + " [" + price / 1000 + "]");
-	} // 메뉴 이름을 프린트하는 함수
-}
-
-//====================================================================================/
-// Menuboard =========================================================================/
-
-class Menuboard {
-	static Menu[] makeInstanceBoards; // 왜 static으로 해야할까? -> static 메서드는 static 멤버 변수만 사용할 수 있다. 어차피 객체 생성도 안하고 사용할
-	// 메서드일텐데
-	// 또한, 이렇게 클래스가 다른 클래스에 들어와있는 경우를 '포함 관계'라고 한다.
-	// 마지막으로, 멤버 변수에다가 메모리 할당하는 것은 기본이 안된 행동이라고 함.
-	static String[] menus = { "Americano", "Latte", "Mocca", "Cappuccino", "Milk Tea", "Chi Tea", "Lemon Sweet",
-			"Jamong Honey" };
-	static int[] prices = { 4100, 4300, 4300, 4800, 5100, 5300, 5800, 6100 };
-
-	private Menuboard() {
-	} // private 생성자 : 외부에서 new 연산자로 객체 생성을 하지 못하도록 조치한다.
-		// Reflection API로 class를 얻어오는 말일 줄 알았는데 그건 아닌듯
-		// private 생성자를 사용했다 -> 인스턴스 생성을 못하게 하겠다 -> 모든 메서드를 static으로 사용해라.
-
-	public static Menuboard makeBoard() { // Menuboard 클래스에 makeBoard 메소드를 정의한다.
-		Menuboard.makeInstanceBoards = new Menu[Menuboard.menus.length]; // 1. Menu 배열을 생성
-		for (int i = 0; i < Menuboard.makeInstanceBoards.length; i++) {
-			Menuboard.makeInstanceBoards[i] = new Menu(); // 2. Menu 배열에 메모리를 할당
-			Menuboard.makeInstanceBoards[i].setId(i + 1);
-			Menuboard.makeInstanceBoards[i].setName(menus[i]);
-			Menuboard.makeInstanceBoards[i].setPrice(prices[i]);
-		}
-//		Menuboard m = new Menuboard();
-		return new Menuboard(); // 결국 return은 Menuboard Type으로 해야해. 그러면 어떻게 해야할까?
-		// 내가 지금 반환할 수 있는 Menuboard가 있나?
-	}
-
-	public static void print() {
-		System.out.println("***** Best Coffee *****");
-		for (int i = 0; i < 8; i++) {
-			Menuboard.makeInstanceBoards[i].print();
-		}
-		System.out.println("***********************");
-	}
-}
-
-//====================================================================================/
-//Main =========================================================================/
-public class Main2 {
-	public static void main(String[] args) {
-		MenuOrder.makeOrder();
-	}
-}
+//
+////		System.out.println(Arrays.toString(order.sizeOfOrders));
+//		order.print();
+//		sc.close();
+//	}
+//}
+//
+////====================================================================================/
+////Menu =========================================================================/
+//
+//class Menu {
+//	private int id; // 메뉴의 번호
+//	private String name; // 메뉴의 이름
+//	private double price; // 메뉴의 가격
+//
+//	public int getId() {
+//		return id;
+//	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public double getPrice() {
+//		return price;
+//	}
+//
+//	public void setPrice(double price) {
+//		this.price = price;
+//	}
+//
+//	public void print() {
+//		System.out.println(id + ": " + name + " [" + price / 1000 + "]");
+//	} // 메뉴 이름을 프린트하는 함수
+//}
+//
+////====================================================================================/
+//// Menuboard =========================================================================/
+//
+//class Menuboard {
+//	static Menu[] makeInstanceBoards; // 왜 static으로 해야할까? -> static 메서드는 static 멤버 변수만 사용할 수 있다. 어차피 객체 생성도 안하고 사용할
+//	// 메서드일텐데
+//	// 또한, 이렇게 클래스가 다른 클래스에 들어와있는 경우를 '포함 관계'라고 한다.
+//	// 마지막으로, 멤버 변수에다가 메모리 할당하는 것은 기본이 안된 행동이라고 함.
+//	static String[] menus = { "Americano", "Latte", "Mocca", "Cappuccino", "Milk Tea", "Chi Tea", "Lemon Sweet",
+//			"Jamong Honey" };
+//	static int[] prices = { 4100, 4300, 4300, 4800, 5100, 5300, 5800, 6100 };
+//
+//	private Menuboard() {
+//	} // private 생성자 : 외부에서 new 연산자로 객체 생성을 하지 못하도록 조치한다.
+//		// Reflection API로 class를 얻어오는 말일 줄 알았는데 그건 아닌듯
+//		// private 생성자를 사용했다 -> 인스턴스 생성을 못하게 하겠다 -> 모든 메서드를 static으로 사용해라.
+//
+//	public static Menuboard makeBoard() { // Menuboard 클래스에 makeBoard 메소드를 정의한다.
+//		Menuboard.makeInstanceBoards = new Menu[Menuboard.menus.length]; // 1. Menu 배열을 생성
+//		for (int i = 0; i < Menuboard.makeInstanceBoards.length; i++) {
+//			Menuboard.makeInstanceBoards[i] = new Menu(); // 2. Menu 배열에 메모리를 할당
+//			Menuboard.makeInstanceBoards[i].setId(i + 1);
+//			Menuboard.makeInstanceBoards[i].setName(menus[i]);
+//			Menuboard.makeInstanceBoards[i].setPrice(prices[i]);
+//		}
+////		Menuboard m = new Menuboard();
+//		return new Menuboard(); // 결국 return은 Menuboard Type으로 해야해. 그러면 어떻게 해야할까?
+//		// 내가 지금 반환할 수 있는 Menuboard가 있나?
+//	}
+//
+//	public static void print() {
+//		System.out.println("***** Best Coffee *****");
+//		for (int i = 0; i < 8; i++) {
+//			Menuboard.makeInstanceBoards[i].print();
+//		}
+//		System.out.println("***********************");
+//	}
+//}
+//
+////====================================================================================/
+////Main =========================================================================/
+//public class Main2 {
+//	public static void main(String[] args) {
+//		MenuOrder.makeOrder();
+//	}
+//}
