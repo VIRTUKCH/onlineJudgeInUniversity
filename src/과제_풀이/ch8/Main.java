3d66deb5187f1e7fad8467688619b381f46429e3
kch4731 <kch4731@naver.com>
2022-11-29 11:08:19 +0900

    Add : 8장 풀이 추가

--- src/과제_풀이/ch8/Main.java
+++ src/과제_풀이/ch8/Main.java
@@ -1,44 +1,101 @@
 package 과제_풀이.ch8;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-enum Vocabulary_MENU {
-	Exit(0), Print_Vocabulary(1), Search_a_word(2), Add_a_new_word(3), Delete_a_word(4);
-
-	Vocabulary_MENU(int value) {
-		this.value = value;
-	}
-
-	private final int value;
-
-	public int value() {
-		return value;
-	}
-}
-
 class Note {
 	public HashMap<String, String> word;
+	Scanner sc;
 
-	public boolean LoadData(String[] voca) throws IOException {
+	Note() {
+		sc = new Scanner(System.in);
+	}
+
+	// 여기에서 단어장에 단어 다 추가해줘야 함.
+	public boolean LoadData(String[] voca) {
 		word = new HashMap<>();
 		if (word.isEmpty()) { // 비었으면 추가하고 true 반환하기
+			// 1. Key와 Value를 알맞게 짝지어서 단어를 추가한다.
+			for (int i = 0; i < voca.length; i++) {
+				String[] strArr = voca[i].split(",", 2); // 컴마를 기준으로 두 개로 나눕니다
+				word.put(strArr[0], strArr[1]); // 컴마를 기준으로 좌우로 나누어서 저장
+			}
 			return true;
 		} else { // 안비었으면 뭔가 이상한거 -> false 반환하기
-			throw new IOException();
+			return false;
 		}
 	}
 
 	void operate() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("========= Vocabulary Menu =========\r\n" + " 1. Print Vocabulary\r\n"
-				+ " 2. Search a word\r\n" + " 3. Add a new word\r\n" + " 4. Delete a word\r\n" + " 0. Exit\r\n"
-				+ "===================================");
-		System.out.print(" ==>> ");
-		int number = sc.nextInt();
-		System.out.println("사용자가 입력한 값  : " + number);
+		while (true) {
+			System.out.println();
+			System.out.println("========= Vocabulary Menu =========\r\n" + " 1. Print Vocabulary\r\n"
+					+ " 2. Search a word\r\n" + " 3. Add a new word\r\n" + " 4. Delete a word\r\n" + " 0. Exit\r\n"
+					+ "===================================");
+			System.out.print(" ==>> ");
+			int number = sc.nextInt();
+			switch (number) {
+			case 1:
+				printVocabulary();
+				break;
+			case 2:
+				searchWord();
+				break;
+			case 3:
+				addNewWord();
+				break;
+			case 4:
+				deleteWord();
+				break;
+			case 0:
+				return; // 0을 입력하면 프로그램을 종료시켜 버린다.
+			}
+		}
 	}
+
+	// case 1:
+	private void printVocabulary() {
+		System.out.println("=================== Vocabulary ===================");
+		System.out.println("amazing : " + word.get("amazing"));
+		System.out.println("chat : " + word.get("chat"));
+		System.out.println("minimal : " + word.get("minimal"));
+		System.out.println("supermarket : " + word.get("supermarket"));
+		System.out.println("wellbeing : " + word.get("wellbeing"));
+		System.out.println("==================================================");
+	}
+
+	// case 2:
+	private void searchWord() {
+		System.out.print("Enter a word to search : ");
+		String searchWord = sc.next();
+		sc.nextLine();
+
+		if (word.get(searchWord) == null) {
+			System.out.println("Your entered word does not exist.");
+		} else {
+			System.out.println(searchWord + " : " + word.get(searchWord));
+		}
+	}
+
+	// case 3:
+	private void addNewWord() {
+		System.out.print("Enter a word to append : ");
+		String wordToAppend = sc.next();
+		sc.nextLine();
+		System.out.print("Meaning : ");
+		String meaningOfWord = sc.nextLine();
+		word.put(wordToAppend, meaningOfWord);
+		System.out.println("Word is appended successfully.");
+	}
+
+	// case 4:
+	private void deleteWord() {
+		System.out.print("Enter a word to remove : ");
+		String wordToRemove = sc.next();
+		word.remove(wordToRemove);
+		System.out.println("The word is removed successfully.");
+	}
+
 }
 
 public class Main {
@@ -50,15 +107,10 @@ public class Main {
 	public static void main(String[] args) {
 		Note note = new Note();
 
-		try {
-			if (note.LoadData(voca)) {
-				System.out.println("Vocabulary is loaded. (Total words : " + note.word.size() + ")");
-			} else {
-				System.out.println("Error : Vocabulary isn't loaded.");
-				return;
-			}
-		} catch (IOException e) {
-			System.out.println("Error : Can't load vocadata.");
+		if (note.LoadData(voca)) {
+			System.out.println("Vocabulary is loaded. (Total words : " + note.word.size() + ")");
+		} else {
+			System.out.println("Error : Vocabulary isn't loaded.");
 			return;
 		}
 		note.operate();
